2d611a73783b1854435dc89d10a8914be3cbc248
Pit <pit@example.com>
2023-11-07 15:22:36 +0100

    Uporządkowano Layout

--- Models/Order.cs
+++ Models/Order.cs
@@ -11,24 +11,24 @@ namespace Shop.Models
         [BindNever]
         public ICollection<CartLine> Lines { get; set; } = new List<CartLine>();
 
-        [Required(ErrorMessage = "Pleae insert Name and Surname")]
+        [Required(ErrorMessage = "Proszę wpisać imię i nazwisko")]
         public string? Name { get; set; }
 
-        [Required(ErrorMessage = "Please insert Address")]
+        [Required(ErrorMessage = "Proszę wpisać adres")]
         public string? Line1 { get; set;}
 
         public string? Line2 { get; set;}
 
         public string? Line3 { get; set;}
 
-        [Required(ErrorMessage ="Please insert Town")]
+        [Required(ErrorMessage ="Proszę wpisać miejscowość")]
         public string? City { get; set;}
 
-        [Required(ErrorMessage = "Please insert Zip Code")]
+        [Required(ErrorMessage = "Proszę wpisać kod pocztowy")]
 
         public string? Zip { get; set;}
 
-        public string? Country { get; set; } = "Poland";
+        public string? Country { get; set; } = "Polska";
 
         public bool GiftWrap { get; set;}
 
